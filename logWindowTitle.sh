479ea03d2ce17102e6c85a507a8d14f4fb528abe
Richard Reijmers <richard@occhio.nl>
2013-06-07 10:46:29 +0200

    Script

---
+++ logWindowTitle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while true
+do
+	# Get current window title
+	currentWindowTitle=`xwininfo -id $(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}') | grep xwininfo | sed 's/xwininfo: Window id: //g' | sed -e 's/0x....... //g' | sed 's/"//g'`
+
+	# Format datestamp
+	currentTime=`date "+%H:%M:%S"`
+	currentDate=`date "+%Y%m%d"`
+
+	# 2013-01-17 15:17:57
+	currentTimestamp=`date "+%Y-%m-%d %H:%M:%S"`
+
+	if [ "${currentWindowTitle}" != "${oldWindowTitle}" ]
+	then
+		echo "${currentTime} ${currentWindowTitle}" >> ~/LOG/`date +%y%m%d`.log
+	fi
+	oldWindowTitle=${currentWindowTitle}
+	sleep 1
+done
